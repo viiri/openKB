f1dcd8b622a6676ffd12b7d2beb6070ded3e1d89
Vitaly Driedfruit <driedfruit@mindloop.net>
2011-09-12 19:48:10 +0400

    Adds GPL blurbs to dos-* files.

--- src/lib/dos-cc.c
+++ src/lib/dos-cc.c
@@ -1,3 +1,22 @@
+/*
+ *  dos-cc.c -- .CC file reader
+ *  Copyright (C) 2011 Vitaly Driedfruit
+ *
+ *  This file is part of openkb.
+ *
+ *  openkb is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  openkb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with openkb.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "kbsys.h"
 #include "kbdir.h"
 
