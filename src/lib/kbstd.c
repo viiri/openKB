4fa167938f4eafbbee21526c71e18d817e356f6c
driedfruit <driedfruit@mindloop.net>
2013-10-02 10:14:02 +0400

    Add KB_strdup function.

--- src/lib/kbstd.c
+++ src/lib/kbstd.c
@@ -266,6 +266,14 @@ void KB_dirncpy(char *dst, const char *src, unsigned int n) {
 	if (l && src[l - 1] == PATH_SEP_SYM) dst[k - 1] = '\0';
 }
 
+char* KB_strdup(const char *src) {
+	int l = strlen(src) + 1;
+	char *d = malloc(sizeof(char) * l);
+	if (d == NULL) return NULL;
+	KB_strncpy(d, src, l);
+	return d;
+}
+
 int file_size(const char *filename) { 
 	struct stat status; 
 	if (stat(filename, &status)) return 0; 
