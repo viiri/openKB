93813c182d7cca2771230cfa80133ef76e1e263c
driedfruit <driedfruit@mindloop.net>
2014-06-22 07:54:23 +0000

    Add license banner to dos-data.c

--- src/lib/dos-data.c
+++ src/lib/dos-data.c
@@ -1,3 +1,23 @@
+/*
+ *  dos-data.c -- DOS module
+ *  Copyright (C) 2011-2014 Vitaly Driedfruit
+ *
+ *  This file is part of openkb.
+ *
+ *  openkb is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  openkb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with openkb.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "kbauto.h"
 #include "kbconf.h"
 #include "kbfile.h"
